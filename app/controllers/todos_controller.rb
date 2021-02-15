d1086530b63e75db7dc795d7d65d349211863b80
saunmanoj888 <manoj@kiprosh.com>
2021-02-15 23:55:57 +0530

    add concern for contoller response to refactor code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,46 +1,37 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
 
   def index
     @todos = Todo.all.includes(:items)
-    render json: @todos
+    json_response(@todos)
   end
 
   def show
-    @todo = Todo.find(params[:id])
-    render json: @todo
+    json_response(@todo)
   end
 
   def create
-    @todo = Todo.new(todo_params)
-    if @todo.save
-      render json: @user
-    else
-      render error: { error: 'Unable to create Todo' }, status: 400
-    end
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
   end
 
   def update
-    @todo = Todo.find(params[:id])
-    if @todo
-      @todo.update(todo_params)
-      render json: { message: 'created Todo' }, status: 200
-    else
-      render error: { error: 'Unable to update Todo' }, status: 400
-    end
+    @todo.update(todo_params)
+    head :no_content
   end
 
   def destroy
-    @todo = Todo.find(params[:id])
-    if @todo.destroy
-      render json: { message: 'Deleted Todo' }, status: 200
-    else
-      render error: { error: 'Unable to delete Todo' }, status: 400
-    end
+    @todo.destroy
+    head :no_content
   end
 
   private
 
   def todo_params
     params.require(:todo).permit(:title)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
   end
 end
